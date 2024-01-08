754ff618a77876db1e8811ae797b3cdbb0520c59
Bukhtiar-Murtaza <bukhti.murtaza@hotmail.co.uk>
2024-01-08 13:40:30 +0000

    delete this

--- source/game.py
+++ source/game.py
@@ -1,10 +1,18 @@
 from extras import Extras
+
+
 class GamePlay:
-    def __init__(self, player_points, health, has_mc_key, player_position):
+    def __init__(self):
         self.player_points = 0
-        self.health = 1
-        self.has_mc_key = False
-        self.player_position = player_position
+        self.player_health = 10
+        self.player_position = "outside_castle"
+        self.guard_1 = True
+        self.has_red_key = False
+        self.has_green_key = False
+        self.dog = True
+        self.red_chest_unlocked = False
+        self.red_chest_open = False
+        self.has_armor = False
         self.slow = Extras()
 
     def gamePlay(self):
@@ -14,7 +22,7 @@ class GamePlay:
             3. Exit
             """)
         while True:
-            gamerselection= input("Enter the index of your choice: ")
+            gamerselection = input("Enter the index of your choice: ")
             if gamerselection == "1":
                 self.starting_prompt()
             elif gamerselection == "2":
@@ -26,80 +34,194 @@ class GamePlay:
                 
                 Invalid Input, Please choose from the above options.""")
 
-
     def starting_prompt(self):
         self.slow.slowPrint("""
-            You are standing at the front door of the castle. You have a rusty sword, it wont defeat the
-            the boss but it'll do the trick against his guards. The door is open, you can enter the castle.
-            What do you want to do ?""")
-        command = input()
+            You are standing at the front of the castle. You have a rusty sword, it wont defeat the
+            the boss but it'll do the trick against his guards. The castle has a door, it is open but
+            a Guard stands in your way.
+            What do you want to do ?
+            HINT: You can give commands to your avatar, try to use simple action words, e.g, if you want
+            your avatar to enter a through a door to a room, give the command 'enter room'
+            """)
+        command = input()  # make command to lower and split function
         try:
             while True:
                 command = command.lower()
                 command_array = command.split()
                 # can use if "word" not in "command"
-                if command_array[0] == 'enter' and command_array[1] == 'castle':
+                if command_array[0] == 'enter' and command_array[1] == 'castle' and self.guard_1 == False:
                     break
+                elif command_array[0] == 'enter' and command_array[1] == 'castle' and self.guard_1 == True:
+                    self.slow.slowPrint("""
+                        You can not enter the castle, the guard is in your way. Defeat him in order to proceed.
+                        What do you want to do ?
+                    """)
+                elif command_array[0] == 'attack' and command_array[1] == 'guard':
+                    if self.guard_1:
+                        self.player_points = self.player_points + 10  # update points, store in file
+                        self.player_health = self.player_health - 1  # update health, store in file
+                        self.guard_1 = False
+                        self.slow.slowPrint("""
+                            The guard is defeated. Your health reduced during combat. What do you want to do ?
+                            """)
+                    else:
+                        self.slow.slowPrint("""
+                            The guard is already defeated. What do you want to ?
+                        """)
                 else:
-                    self.slow.slowPrint("Invalid command, try again.")
-                    command = input()
+                    self.slow.slowPrint("""
+                        Invalid command, try again.
+                        """)
+                command = input()
         except:
             self.slow.slowPrint("An infinite loop exception has occurred")
 
     def common_chamber(self):
+        self.player_position = "common_chamber"
         self.slow.slowPrint("""
             You have entered the castle. You are standing in the Common Chamber.
-            There are 3 doors on the side,
-            door 1, door 2, door3, and one in the front leading to the Middle Chamber.
+            This is a safe zone, you can save your game here. 
+            There is a table next to you in this room, with two keys lying on it. One key is green and the other is red.
+            There is a door in front of you which leads to the Middle Chamber.
             What do you want to do ?""")
         command = input()
         try:
             while True:
                 command = command.lower()
                 command_array = command.split()
-                if command_array[0] == 'enter' and command_array[1] == 'middle' and command_array[2] == 'chamber':
-                    if self.has_mc_key == True:
-                        break
-                    else:
-                        self.slow.slowPrint("""
-                            The door is locked, it can only be opened with a key, which you do not possess.
-                            Hint: The key is somewhere in the Common Chamber.
-                            What do you want to do ?""")
-                        command = input()
-                elif command_array[0] == 'enter' and command_array[1] == 'door' and command_array[2] == '1':
-                    self.slow.slowPrint("The door opens to a room, there is a guard in here. What do you want to do.")
-                    command = input()
-                    command = command.lower()
-                    command_array = command.split()
-                    if command_array[0] == 'attack' and command_array[1] == 'guard':
-                        self.slow.slowPrint("Guard is defeated")
-                        #Guard status will be updated
-                        self.has_mc_key = True
-                        self.player_points = self.player_points + 10
-                        self.slow.slowPrint("""
-                            The guard drops a key, you pick up the key and can now access the Middle Chamber.
-                            What do you want to do ?""")
-                        command = input()
-                        command = command.lower()
-                        command_array = command.split()
-                        if command_array[0] == 'leave' and command_array == 'room':
-                            self.slow.slowPrint("You are now in the Common Chamber. What do you want to do ?")
-                            command = input()
-                    else:
-                        self.slow.slowPrint("Invalid command, try again.")
-                        command = input()
-                elif command_array[0] == 'enter' and command_array[1] == 'door' and command_array[2] == '2':
+                if command_array[0] == 'save' or (command_array[0] == 'save' and command_array[1] == 'game'):
                     self.slow.slowPrint("""
-                        You enter a room, there is a crystal in this room. You can pick it up.
-                        What do you want to do ?""")
-                    if command_array[0] == 'leave' and command_array == 'room':
-                        self.slow.slowPrint("You are now in the Common Chamber. What do you want to do ?")
-                        command = input()
-                elif command_array[0] == 'enter' and command_array[1] == 'door' and command_array[2] == '3':
-                    self.slow.slowPrint("""You enter a room, there is an orb inside.
-                          But you must complete a riddle in order to obtain it.""")
+                        Add functionality, Game saved. What do you want to do ?
+                    """)  # save username, avatar, location, health, points, keys, weapon, orb, potion, timestamp
+                elif command_array[0] == 'i':
+                    self.slow.slowPrint("""
+                        Display inventory, read from file.
+                    """)
+                elif command_array[0] == 'enter' and command_array[1] == 'middle' and command_array[2] == 'chamber':
+                    break
+                elif command_array[0] == 'take' and command_array[1] == 'red' and command_array[2] == 'key':
+                    if not self.has_red_key:
+                        self.has_red_key = True  # update inventory, store in file
+                        self.slow.slowPrint("""
+                            You have taken the red key. What do you want to do ?
+                        """)
+                    else:
+                        self.slow.slowPrint("""
+                            You have already taken the red key. What do you want to do ?
+                        """)
+                elif command_array[0] == 'take' and command_array[1] == 'green' and command_array[2] == 'key':
+                    if not self.has_green_key:
+                        self.has_green_key = True  # update inventory, store in file
+                        self.slow.slowPrint("""
+                            You have taken the green key. What do you want to do ?
+                        """)
+                    else:
+                        self.slow.slowPrint("""
+                            You have already taken the green key. What do you want to do ?
+                        """)
+                elif command_array[0] == 'take':
+                    if self.has_red_key and self.has_green_key:
+                        self.slow.slowPrint("""
+                            There is nothing here to take.
+                        """)
+                    else:
+                        self.slow.slowPrint("""
+                            Take what ?
+                        """)
                 else:
                     self.slow.slowPrint("Invalid command, try again.")
-                    command = input()
+                command = input()
+        except:
+            self.slow.slowPrint("An infinite loop exception has occurred")
+
+    def middle_chamber(self):
+        self.player_position = "middle_chamber"
+        ask_red_key = False
+        self.slow.slowPrint("""
+            You are standing in the Middle Chamber. There is a red chest here guarded by a dog.
+            There is a door in front of you leading to the Main Chamber.
+            What do you want to do ? 
+        """)
+        command = input()
+        try:
+            while True:
+                command = command.lower()
+                command_array = command.split()
+                if command_array[0] == 'enter' and command_array == 'main' and command_array[2] == 'chamber':
+                    break
+                elif command_array[0] == 'attack' and command_array[1] == 'dog':
+                    if self.dog:
+                        self.dog = False
+                        self.player_points = self.player_points + 10
+                        self.player_health = self.player_health - 1
+                        self.slow.slowPrint("""
+                            You defeated the dog, your health reduced during combat.
+                            What do you want to do ?
+                        """)
+                    else:
+                        ask_red_key = False
+                        self.slow.slowPrint("""
+                            The dog has already been defeated. What do you want to do ?
+                        """)
+                elif command_array[0] == 'unlock' and command_array[1] == 'red' and command_array[2] == 'chest':
+                    if self.dog:
+                        self.slow.slowPrint("""
+                            The dog bit you, you can not unlock or open the chest without defeating the dog.
+                            What do you want to do ?
+                        """)
+                    elif self.red_chest_unlocked:
+                        self.slow.slowPrint("""
+                            The red chest is already unlocked. What do you want to do ?
+                        """)
+                    else:
+                        self.slow.slowPrint("""
+                            Which key do you want to use ?
+                        """)
+                        ask_red_key = True
+                elif command_array[0] == 'red' and command_array[1] == 'key' and ask_red_key:
+                    ask_red_key = False
+                    self.red_chest_unlocked = True
+                    self.slow.slowPrint("""
+                        You used the red key to unlock the red chest. What do you want to do ?
+                    """)
+                elif command_array[0] == 'green' and command_array[1] == 'key' and ask_red_key:
+                    ask_red_key = False
+                    self.slow.slowPrint("""
+                        The green key does not unlock the red chest. Nothing happens.
+                        What do you want to do ?
+                    """)
+                elif ((command_array[0] == 'open' and command_array[1] == 'chest') or
+                      (command_array[0] == 'open' and command_array[1] == 'red' and command_array[2] == 'chest')):
+                    if self.red_chest_unlocked:
+                        self.red_chest_open = True
+                        self.slow.slowPrint("""
+                            You open the chest and find a piece of Armor. What do you want to do ?
+                        """)
+                    elif self.dog:
+                        self.slow.slowPrint("""
+                            The dog bit you, you can not unlock or open the chest without defeating the dog.
+                            What do you want to do ?
+                        """)
+                    elif self.red_chest_open:
+                        self.slow.slowPrint("""
+                            It is already open. What do you want do ?
+                        """)
+                    elif not self.red_chest_unlocked:
+                        self.slow.slowPrint("""
+                            The chest is locked, you can open it with a key.
+                            What do you want to do ?
+                        """)
+                elif command_array[0] == 'take' and command_array[1] == 'armor' and self.red_chest_open:
+                    self.has_armor = True
+                    self.slow.slowPrint("""
+                        You have taken and worn the Armor. It will help you to defeat the Boss.
+                        What do you want to do ?
+                    """)
+                else:
+                    ask_red_key = False
+                    self.slow.slowPrint("""
+                        Invalid command, try again.
+                    """)
+                command = input()
         except:
             self.slow.slowPrint("An infinite loop exception has occurred")
--- source/main.py
+++ source/main.py
@@ -7,13 +7,14 @@ class Main(object):
     def __init__(self):
         self.register = PlayerRegistration()
         self.intro = Introduction()
-        self.game = GamePlay(0,1,False, True)
+        self.game = GamePlay()
 
     def main(self):
         self.intro.img()
         self.intro.introScenario()
         self.register.welcomePage()
         self.game.gamePlay()
+        self.game.starting_prompt()
 
 
 
